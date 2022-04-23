a9e8497725bfd2fd7905480da04b99141730c66f
Agata <amomot@bio-prodict.nl>
2022-04-23 15:04:52 +0200

    Started writing registers

--- so_emulator.asm
+++ so_emulator.asm
@@ -3,10 +3,19 @@ section .rodata
 align 16
 jump: dq procedure1, procedure1, procedure2
 
+section .bss
+align 8
+A: resb 1
+D: resb 1
+X: resb 1
+Y: resb 1
+PC: resb 1
+C: resb 1
+Z: resb 1 	;SETcc instructions!
+
 section .text
+
 so_emul:
-;	push .procedure1
-;	push .procedure2
 	jmp [rel jump + 16]
 
 procedure1:
--- so_emulator_example.c
+++ so_emulator_example.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int so_emul();
+int so_emul(uint16_t* code);
+uint16_t code[5] = {
+  0x4000 + 0x100 * 2 + 1,           // MOVI X, 1
+  0x4000 + 0x100 * 3 + 0,           // MOVI Y, 0
+  0x0000 + 0x100 * 0 + 0x0800 * 5};  // MOV  A, [Y]
 
 int main() {
 	printf("before\n");
-	int res = so_emul();
+	int res = so_emul(code);
 	printf("after\n");
 	printf("RES: %d\n", res);
 
