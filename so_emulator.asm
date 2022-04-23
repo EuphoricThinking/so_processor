43abd8dd049e9f62d8caeb8738e073e8a0f89033
Agata <amomot@bio-prodict.nl>
2022-04-24 00:33:22 +0200

    First one may be an address, second one must be a value

--- so_emulator.asm
+++ so_emulator.asm
@@ -42,13 +42,15 @@ PC: resb 1
 C: resb 1
 Z: resb 1 	;SETcc instructions!
 
-;testtab: resb 4
+testtab: resb 4
 
 section .text
 
 so_emul:
 	lea rcx, [rel instructions]
 
+	and byte[rel testtab + 1], 8
+	ret
 ;	lea r9, [rel testtab + 2]
 ;	mov r9b, byte[r9]
 ;	mov byte [rel testtab + 1], r9b
