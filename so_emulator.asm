f00a19a0a8b1e36ae368c06a8134246bb6047eee
Agata <amomot@bio-prodict.nl>
2022-04-24 00:11:28 +0200

    Found move combination which should work

--- so_emulator.asm
+++ so_emulator.asm
@@ -42,10 +42,16 @@ PC: resb 1
 C: resb 1
 Z: resb 1 	;SETcc instructions!
 
+testtab: resb 4
+
 section .text
 
 so_emul:
 	lea rcx, [rel instructions]
+	lea r9, [rel testtab + 2]
+	mov r9b, byte[r9]
+	mov byte [rel testtab + 1], r9b
+	ret
 ;	mov rax, 1
 ;	shl rax, 8
 ;	shr rax, 8
