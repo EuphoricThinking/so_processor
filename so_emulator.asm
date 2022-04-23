086dec868242c6626a212481ba940ff8ad516787
Agata <amomot@bio-prodict.nl>
2022-04-23 04:21:44 +0200

    At least compiles

--- so_emulator.asm
+++ so_emulator.asm
@@ -1,16 +1,20 @@
 global so_emul
+section .data
+align 16
+jump: dq procedure1, procedure2
 
+section .text
 so_emul:
-	push .procedure1
-	push .procedure2
-	call qword [rsp + 8]
+;	push .procedure1
+;	push .procedure2
+	jmp [rel jump + 8]
 
-.procedure1:
+procedure1:
 	mov rax, 3
 	sub rsp, 16
 	ret
 
-.procedure2:
+procedure2:
 	mov rax, 4
 	sub rsp, 16
 	ret
