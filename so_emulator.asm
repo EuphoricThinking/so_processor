f2948973ef3ede9bf959225a64859b2a1a18fb7e
Agata <amomot@bio-prodict.nl>
2022-04-25 23:41:56 +0200

    Added comments; wants to add smart overflow

--- so_emulator.asm
+++ so_emulator.asm
@@ -27,12 +27,14 @@ FOURTH_GR_ADDR_CONST equ 15
 
 XCHG_CODE equ 8
 
+; Codes for distinction whether argument is a register, memory address
+; or a memory address produced by addition of register values
 MEM_ADDR_CODE equ 4
 X_Y_PLUS_CODE equ 2
 X_Y_BIAS equ 2
 
 SPINLOCK_OWNED equ 1
-SPINLOCK_XCHG equ 2
+SPINLOCK_XCHG equ 2      ; mocking acquired spinlock for lack of atomicity in xchg
 
 CLEAR_LEFT_A1 equ 5
 CLEAR_RIGHT_AFTER_LEFT equ 13
@@ -42,7 +44,6 @@ CLEAR_RIGHT_A2 equ 11
 
 section .rodata
 align 16
-jump: dq procedure1, procedure1, procedure2
 instructions: dq MOV, EMPT, OR, EMPT, ADD, SUB, ADC, SBB, \
 		 MOVI, \
 		 CLC, STC, \
@@ -50,26 +51,30 @@ instructions: dq MOV, EMPT, OR, EMPT, ADD, SUB, ADC, SBB, \
 		 EMPT, JNC, JC, JNZ, JZ
 
 section .bss
-align 8		; A D X Y PC C Z
+; A D X Y PC C Z
+; Filled with zeros memory for cores data sets
+align 8
 state: resb 8*CORES
 
 alignb 4
 spin_lock: resd 1
-					;SETcc instructions!
 
-;testtab: resb 4
-;cur_proc: resq 1
-
-;section .data
-;cur_proc: dq 1
 
 section .text
+
+; r12 - status of ownership of the spinlock for the current core
+; r13 - temporary exchange register for spinlock
+; r11 - address of the instruction table
+; r10 - the first argument
+; r9 - the second argument or imm8
+; r8 - temporary; resulting address storage for argument 1-2 evaluation
+; rbx - address to jump from the procedure for arguments 1-2 evaluation
+; rcx - table address for the current core
 
 so_emul:
     push rbx
     push r12
     push r13
-;    push r14
 
     xor r12, r12 ; Current thread doesn't use spinlock
     mov r13d, 1  ; temporary spinlock flag
@@ -93,7 +98,7 @@ check_steps:
     test r12, r12
     jz .clean_spinlock
 
-    mov dword[rel spin_lock], r13d ; change to 0?
+    mov dword[rel spin_lock], r13d ; assumed r13d = 0
     mov r13d, 1
     xor r12, r12
 
@@ -102,12 +107,12 @@ check_steps:
 	jz .no_steps_left
 
 	movzx r10, byte[rcx + PC_IND]
-	mov r10w, word[rdi + 2*r10]   ; a value from code
+	mov r10w, word[rdi + 2*r10]   ; a value from code - program memory
 
 	dec rdx
 	inc byte [rcx + PC_IND]
 
-	cmp r10w, 0xFFFF
+	cmp r10w, 0xFFFF ; BRK
 	je .no_steps_left
 
 	movzx rax, r10w ; a value to operate on
@@ -130,45 +135,38 @@ check_steps:
 	jmp check_steps
 
 .first_group:
-;	lea r10, [rel check_steps.first_r10]
-;	mov qword[rel cur_proc], r10
-
     cmp al, XCHG_CODE
     je XCHG
 
     lea rbx, [rel check_steps.first_r10]
 
-	mov r8, rax ; arg1      ; r10
-	shl r8w, CLEAR_LEFT_A1   ; clear arg2 from left bits
-	shr r8w, CLEAR_RIGHT_AFTER_LEFT
+	mov r8, rax                     ; arg1      ; r10
+	shl r8w, CLEAR_LEFT_A1          ; clear arg1 from left bits
+	shr r8w, CLEAR_RIGHT_AFTER_LEFT ; clear arg1 from right bits
 
 	jmp read_address_of_arg_val
 
 .first_r10:
 	mov r10, r8  ; arg1, address
 
-;	lea r9, [rel check_steps.first_r9]
-;	mov qword[rel cur_proc], r9
     lea rbx, [rel check_steps.first_r9]
 
 	mov r8, rax  ; arg2     ; r9
-	shr r8w, CLEAR_RIGHT_A2  ; nothing before
+	shr r8w, CLEAR_RIGHT_A2  ; nothing before - empty left bits
 
 	jmp read_address_of_arg_val
 
 .first_r9:
 	mov r9b, byte[r8]   ; arg2 - insert value, not address
 
-	movsx rax, al
-;	ret
+	movzx rax, al
+
 	jmp [r11 + 8*rax]
 
 .second_group:
-;	lea r10, [rel check_steps.second_r10]
-;	mov qword[rel cur_proc], r10
     lea rbx, [rel check_steps.second_r10]
 
-	mov r8, rax  ; arg1   ; r10
+	mov r8, rax                     ; arg1   ; r10
 	shl r8w, CLEAR_LEFT_A1
 
 	shr r8w, CLEAR_RIGHT_AFTER_LEFT
@@ -176,9 +174,9 @@ check_steps:
 	jmp read_address_of_arg_val
 
 .second_r10:
-	mov r10, r8  ; arg1, address
+	mov r10, r8                     ; arg1, address
 
-	mov r9b, al  ; imm8, value
+	mov r9b, al                     ; imm8, value
 
 	shl ax, CLEAR_LEFT_A2
 	shr ax, CLEAR_RIGHT_AFTER_LEFT
@@ -201,11 +199,8 @@ check_steps:
 	jmp [r11 + 8*(rax + FOURTH_GR_ADDR_CONST)]
 
 .no_steps_left:
-;	mov byte [rel state + C_IND], 1
-;	mov byte [rel state + Z_IND], 1
 	mov rax, [rcx]
 
-;    pop r14
     pop r13
 	pop r12
 	pop rbx
@@ -220,10 +215,9 @@ check_steps:
 read_address_of_arg_val:
 	test r8b, MEM_ADDR_CODE
 	jnz .x_y_test
-;    jnz .spinlock_wait
 
-	lea r8, [rcx + r8]
-;	jmp [rel cur_proc]
+	lea r8, [rcx + r8]  ; register address
+
     jmp rbx
 
 .x_y_test:
@@ -242,10 +236,11 @@ read_address_of_arg_val:
 	test r8b, X_Y_PLUS_CODE
 	jnz .x_y_plus
 
-	and r8b, 1
+    ; value under the address which is the value of register X or Y
+	and r8b, 1   ; codes differ at one bit
 	movzx r8, byte[rcx + X_Y_BIAS + r8] ; it's uint8_t, unsigned; move x or y to register
 	lea r8, [rsi + r8]
-;	jmp [rel cur_proc]
+
     jmp rbx
 
 .x_y_plus:
@@ -253,16 +248,9 @@ read_address_of_arg_val:
 	movzx r8, byte[rcx + X_Y_BIAS + r8] ; move x or y to register
 	add r8b, byte[rcx + D_IND]
 	lea r8, [rsi + r8]
-;   jmp [rel cur_proc]
+
     jmp rbx
 
-procedure1:
-	mov rax, 3
-	jmp check_steps
-
-procedure2:
-	mov rax, 4
-	jmp check_steps
 
 MOV:
 	mov byte[r10], r9b
@@ -338,6 +326,7 @@ ADDI:
 
 	jmp check_steps
 
+; Empty procedure for inorrect codes and filling the empty indexes in the instruction table
 EMPT:
 	jmp check_steps
 
@@ -424,57 +413,56 @@ BRK:
 ;SPINLOCK_OWNED equ 1
 ;SPINLOCK_XCHG equ 2
 XCHG:
-;    jmp check_steps
 
-    mov r10, rax ; arg1
-    shl r10w, CLEAR_LEFT_A1   ; clear arg1 from left bits
+    mov r10, rax                ; arg1
+    shl r10w, CLEAR_LEFT_A1     ; clear arg1 from left bits
     shr r10w, CLEAR_RIGHT_AFTER_LEFT
 
-    mov r9, rax  ; arg2     ; r9
+    mov r9, rax                 ; arg2     ; r9
     shr r9w, CLEAR_RIGHT_A2
 
+    ; If the arg1 bit which is set for codes of arguments as addresses
+    ; is not set, xchg is non-atomic
     test r10b, MEM_ADDR_CODE
     jz .non_atomic
 
+    ; Reversely, the mentioned bit should not be set for arg2
     test r9b, MEM_ADDR_CODE
     jnz .non_atomic
 
     jmp .get_args
 
 .non_atomic:
+    ; Mocks spinlock acquirement; since read_address_of_arg_val test only for
+    ; non-zero values, we can use different indicator values for different
+    ; purposes, leaving zero for unacquired spinlocks
     mov r12, SPINLOCK_XCHG
 
 .get_args:
     mov r8, r10
     lea rbx, [rel XCHG.xchg_r10]
-    jmp read_address_of_arg_val ; spinlock is acquired in this function,
+    jmp read_address_of_arg_val ; spinlock is acquired in this function or ignored due to set r12
 
 .xchg_r10:
     mov r10, r8
 
     mov r8, r9
     lea rbx, [rel XCHG.xchg_r9]
-    jmp read_address_of_arg_val ; spinlock is acquired in this function,
+    jmp read_address_of_arg_val ; spinlock is acquired in this function or ignored due to set r12
 
 .xchg_r9:
-    ; r8 to r9
-    ; r10 to r8
-    mov r9b, byte[r8]           ; r8 stores address; r9 is temporary address
-    mov al, byte[r10]       ; r14 stores value
 
-    ;mov r8b, byte[r8]
-    mov byte[r10], r9b  ;r8b to r9b
+    mov r9b, byte[r8]       ; r8 stores address; r9 is a storage for value
+    mov al, byte[r10]       ; r10 stores address; al is a storage for value
 
-;    mov r10b, byte[r10]
-;    mov byte[r89], r10b
-    mov byte[r8], al
-
-;    movzx r9, byte[r9]
-;    movzx r10, byte[r10]
+    ; Addresses in registers don't change, only values
+    mov byte[r10], r9b      ; value at r8  -> value at r10
+    mov byte[r8], al        ; value at r10 -> value at r8
 
     cmp r12, SPINLOCK_XCHG
     jne check_steps
 
+    ; Clears the mocked flag
     xor r12, r12
 
 	jmp check_steps
